ec05c0ed5f8710baf5bdcb290d29f8b7f9f01d0a
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-09-11 14:08:04 -0400

    Define initial interfaces

---
+++ morphir/interop/zio/schema/org/finos/morphir/interop/zio/schema/datamodel.scala
@@ -0,0 +1,12 @@
+package org.finos.morphir.interop.zio.schema
+import dev.zio.schema.codec._
+trait MorphirDataModelZioSchemaModule
+  extends MorphirDataModelConceptZioSchemaModule with MorphirDataModelDataZioSchemaModule {
+
+}
+trait MorphirDataModelConceptZioSchemaModule {
+
+}
+trait MorphirDataModelDataZioSchemaModule {
+
+}
---
+++ morphir/interop/zio/schema/org/finos/morphir/interop/zio/schema/ir.scala
@@ -0,0 +1,10 @@
+package org.finos.morphir.interop.zio.schema
+
+import dev.zio.schema
+trait MorphirIrZioSchemaModule extends MorphirIrTypeZioSchemaModule {
+
+}
+
+trait MorphirIrTypeZioSchemaModule { self: MorphirIrZioSchemaModule =>
+
+}
